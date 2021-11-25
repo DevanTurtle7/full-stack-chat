37b0048861e8dc3dccf0bc87d27b304be799435b
Devan Kavalchek <devankav7@gmail.com>
2021-11-25 02:00:50 -0500

    added dynamic rendering

--- src/Chat.js
+++ src/Chat.js
@@ -3,13 +3,34 @@ import { Col } from 'reactstrap';
 import Bubble from './Bubble';
 
 class Chat extends Component {
+    constructor(props) {
+        super(props)
+
+        // Temp stuff for testing
+        this.messages = [
+            {sender: 1, message: "hello"},
+            {sender: 1, message: "Lorem ipsum..."},
+            {sender: 1, message: "This is a long message. This is a test to see what a long message looks like. Lets try it out."},
+            {sender: 2, message: "Hey whats up. This is a long message from the other user"}
+        ]
+        
+        this.userId = 1;
+    }
+
     render() {
+        var messages = []
+
+        for (var i in this.messages) {
+            let message = this.messages[i]
+
+            messages.push(<Bubble
+                sent={message.sender === this.userId}
+                text={message.message}
+            />)
+        }
+
         return (
-            <Col className="mx-1">
-                <Bubble sent={true} text={"Hello"}/>
-                <Bubble sent={true} text={"This is a long message. This is a test to see what a long message looks like. Lets try it out."}/>
-                <Bubble sent={false} text={"Hey whats up. This is a long message from the other user."}/>
-            </Col>
+            <Col className="mx-1">{messages}</Col>
         )
     }
 }
