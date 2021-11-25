7b491fe5ed0314cb3ee82c9564582c656159c222
Devan Kavalchek <devankav7@gmail.com>
2021-11-25 01:22:27 -0500

    stubbed basic layout

---
+++ src/Chat.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import { Col } from 'reactstrap';
+
+class Chat extends Component {
+    render() {
+        return (
+            <Col>
+                <p>Hello</p>
+            </Col>
+         )
+    }
+}
+
+export default Chat;
---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import { Col } from 'reactstrap';
+
+class Sidebar extends Component {
+    render() {
+        return (
+            <Col>
+                <p>Hello</p>
+            </Col>
+         )
+    }
+}
+
+export default Sidebar;
