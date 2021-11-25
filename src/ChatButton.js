e261ea1d3ad0da39aad7b8d87d1ebb1982e1fb71
Devan Kavalchek <devankav7@gmail.com>
2021-11-25 02:45:14 -0500

    stubbed sidebar

---
+++ src/ChatButton.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import { Col } from 'reactstrap';
+import Bubble from './Bubble';
+
+class ChatButton extends Component {
+    render() {
+        return (
+            <Col>
+                <p>User1</p>
+                <p>Okay bye</p>
+            </Col>
+        )
+    }
+}
+
+export default ChatButton;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,12 @@
 import { Component } from 'react';
 import { Col } from 'reactstrap';
+import ChatButton from './ChatButton';
 
 class Sidebar extends Component {
     render() {
         return (
             <Col sm={4} className="sidebar">
-                <p>Hello</p>
+                <ChatButton/>
             </Col>
          )
     }
