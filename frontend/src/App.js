d9db54d089010d1de1207acd3693abafa89dd193
Devan Kavalchek <devankav7@gmail.com>
2021-11-25 20:48:00 -0500

    added state to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,29 @@ import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
 import { Row, Container } from 'reactstrap';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Container fluid>
-        <Row className="row-height">
-          <Sidebar />
-          <Chat />
-        </Row>
-      </Container>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: null,
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Container fluid>
+          <Row className="row-height">
+            <Sidebar />
+            <Chat />
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
