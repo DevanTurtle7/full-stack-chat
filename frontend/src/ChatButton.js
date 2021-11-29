4c1dbb1b3d94ef418ddea63828eca7c07125507b
Devan Kavalchek <devankav7@gmail.com>
2021-11-29 14:16:22 -0500

    added comments

--- frontend/src/ChatButton.js
+++ frontend/src/ChatButton.js
@@ -1,7 +1,20 @@
+/**
+ * A button on the sidebar that opens a chat
+ * 
+ * PROPS:
+ *  name: The name of this chat. If it is a direct message, the name of the user.
+ *  lastMessage: The most recent message that was sent in this chat.
+ *  callback: A callback to the parent, that tells it when this button has been clicked.
+ * 
+ * @author Devan Kavalchek
+ */
 import { Component } from 'react';
 import { Col } from 'reactstrap';
 
 class ChatButton extends Component {
+    /**
+     * Called when this button is clicked. Calls the callback to the parent
+     */
     onClick = () => {
         this.props.callback(this.props.name)
     }
